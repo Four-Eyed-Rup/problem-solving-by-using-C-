868b3dacecd78914dd4b44af3ae8b34b6acb4059
Swarup Das <dasswarup20182001@gmail.com>
2026-09-20 22:41:14 +0530

    Add files via upload

---
+++ showDigits.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number, digit;
+    cout << "Enter a number: ";
+    cin >> number;
+    while(number != 0){
+        digit = number % 10;
+        cout << digit << "\n";
+        number /= 10;
+    }
+    return 0;
+}
